e06f4a1432fe82724721909da567e91c3f448d90
Hridoy Chowdhury <t.okay383@gmail.com>
2026-06-13 22:55:31 +0600

    database collection or post and get route create

--- index.js
+++ index.js
@@ -2,14 +2,88 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT;
+
 
 // middleware
 app.use(cors())
 app.use(express.json())
 
+// MongoDB Client
+const client = new MongoClient(process.env.MONGO_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        // collection create
+        const database = client.db("hireloop-database");
+        const jobCollection = database.collection("jobs");
+        const companyCollection = database.collection("companies");
+
+        // route create 
+        // NO: 01 - POST route
+        app.post('/api/jobs', async (req, res) => {
+
+            try {
+                const jobData = req.body;
+
+                const result = await jobCollection.insertOne(jobData)
+
+                res.status(201).send({
+                    success: true,
+                    message: "Job Created Successfully",
+                    insertedId: result.insertedId
+                })
+
+            } catch (error) {
+
+                console.log('Post Api error', error);
+
+                res.status(500).send({
+                    success: false,
+                    message: "Failed to create new job"
+                })
+            }
+        })
+
+        // get route
+        app.get('/api/jobs', async (req, res) => {
+
+            const query = {};
+
+            if (req.query.companyId) {
+                query.companyId = req.query.companyId;
+            }
+            if (req.query.status) {
+                query.status = req.query.status;
+            }
+
+            const cursor = jobCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+
+        })
+
+
+
+
+
+        console.log("MongoDB Connected Successfully");
+    } catch (error) {
+        console.log('database error:', error)
+    }
+}
+run().catch(console.dir);
 
 // route create 
 app.get('/', (req, res) => {
